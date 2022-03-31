d08f62fb76659e70f2edbe213c13552ae6c15c80
lucilakalbermatter <lucilakalbermatter@hotmail.com>
2022-03-31 09:13:31 +0200

    changes in Department Controller, Role and User

--- src/main/java/finalproject/controllers/UserController.java
+++ src/main/java/finalproject/controllers/UserController.java
@@ -1,4 +1,13 @@
 package finalproject.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+
+
 }
