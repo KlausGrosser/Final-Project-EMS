556fb088ea2ae6c958b4613299227d92dc11c0d1
KlausGrosser <klaus.grosser@deliveryhero.com>
2022-04-01 10:07:17 +0200

    Added CLI to add first employee

--- src/main/java/finalproject/FinalProjectEmsApplication.java
+++ src/main/java/finalproject/FinalProjectEmsApplication.java
@@ -1,7 +1,18 @@
 package finalproject;
 
+import finalproject.models.Employee;
+import finalproject.models.Role;
+import finalproject.repositories.EmployeeRepository;
+import finalproject.repositories.RoleRepository;
+import finalproject.services.EmployeeService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class FinalProjectEmsApplication {
@@ -14,4 +25,34 @@ public class FinalProjectEmsApplication {
         SpringApplication.run(FinalProjectEmsApplication.class, args);
     }
 
+    /**
+     * this populates the roles with basic roles for admins and users
+     */
+    @Bean
+    public CommandLineRunner loadData(
+            RoleRepository roleRepository,
+            EmployeeRepository employeeRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder,
+            EmployeeService employeeService
+    ) {
+        return (args) -> {
+            roleRepository.save(new Role("ROLE_USER"));
+            roleRepository.save(new Role("ROLE_ADMIN"));
+            roleRepository.save(new Role("ROLE_SUPER_ADMIN"));
+
+            Employee e1 = new Employee(
+                    "Rachel",
+                    "Adams",
+//                    LocalDate.of(1980, 1, 1),
+                    "test@gmail.com",
+                    bCryptPasswordEncoder.encode("test"),
+                    roleRepository.findByName("ROLE_SUPER_ADMIN")
+            );
+            employeeService.addEmployee(e1);
+
+            // a list of teachers for saving!
+            //List.of(t1, t2, t3).forEach(teacher -> teacherService.addRole(teacher));
+            //employeeRepository.saveAll(List.of(t1, t2, t3));
+        };
+    }
 }
