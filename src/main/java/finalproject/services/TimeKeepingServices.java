228007ab3469dc87afe191fea4e91bb2f29b662a
AliceM80 <a.mutke@hotmail.de>
2022-04-05 11:06:48 +0200

    remove the TimeKeepingClasses

--- src/main/java/finalproject/services/TimeKeepingServices.java
+++
@@ -1,9 +0,0 @@
-package finalproject.services;
-
-import finalproject.models.TimeKeeping;
-
-import java.util.Set;
-
-public interface TimeKeepingServices  extends IService<TimeKeeping,String> {
-  Set<TimeKeeping> getAll();
-}
