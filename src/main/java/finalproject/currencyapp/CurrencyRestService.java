a9df23d95a897c1091f93e4536a7f064e5dc1644
AliceM80 <a.mutke@hotmail.de>
2022-04-07 15:17:32 +0200

    add a dropdown menu for the currency

--- src/main/java/finalproject/currencyapp/CurrencyRestService.java
+++ src/main/java/finalproject/currencyapp/CurrencyRestService.java
@@ -12,21 +12,22 @@ public class CurrencyRestService {
   private String apiKey = "055fe50ac6aa8f7680c85af3";
 
 
-  public CurrencyRestService(){webClient = WebClient.builder()
+  public CurrencyRestService() {
+    webClient = WebClient.builder()
             .baseUrl("https://v6.exchangerate-api.com/v6/" + apiKey)
             .build();
   }
 
 
-  public Double getConvertedAmount (String base_code, String target_code, double amount){
+  public Double getConvertedAmount(String base_code, String target_code, double amount) {
 
     return (Double) webClient
             .get()
             .uri(uriBuilder -> uriBuilder
                     .path("/pair")
-                    .path("/" +base_code)
-                    .path("/" +target_code)
-                    .path("/" +amount)
+                    .path("/" + base_code)
+                    .path("/" + target_code)
+                    .path("/" + amount)
                     .queryParam("")
                     .build()
             )
@@ -35,30 +36,5 @@ public class CurrencyRestService {
             .map(map -> map.get("conversion_result"))
             .block();  /// returns the body as an array of User
 
-  }
-
-/*  public Set<String> getCurrencyNames() {
-    List<Currency> allCourses = currencyRepository.findAll();
-    Set<String> coursesNames = new HashSet<>();
-    for (Course course : allCourses) {
-      coursesNames.add(course.getCourseName());
-    }
-    return coursesNames;*/
-
-  public Set<String, String> getAvailableCurrencies() {
-    Locale[] locales = Locale.getAvailableLocales();
-
-    // We use TreeMap so that the order of the data in the map sorted
-    // based on the country name.
-    Map<String, String> currencies = new TreeMap<>();
-    for (Locale locale : locales) {
-      try {
-        currencies.put(locale.getDisplayCountry(),
-                Currency.getInstance(locale).getCurrencyCode());
-      } catch (Exception e) {
-        // when the locale is not supported
-      }
-    }
-    return currencies;
   }
 }
